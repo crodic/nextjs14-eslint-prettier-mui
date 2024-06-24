efe0b68189e805618d9bac60ed52a174f19a0cd2
Crodic <113219638+Crodic@users.noreply.github.com>
2024-06-24 09:38:25 +0700

    Initial commit from Create Next App

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    semi: false,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    trailingComma: 'es5',
+    singleAttributePerLine: true,
+    printWidth: 120,
+    tabWidth: 4,
+}
